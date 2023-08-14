3e55e4cbc6c6f38544ea7c70b86d467b8d7e9190
soni-shashan <140899664+soni-shashan@users.noreply.github.com>
2023-08-14 16:20:00 +0530

    Add files via upload

---
+++ ARRAY EXAMPLES/array_ascending.C
@@ -0,0 +1,35 @@
+# include<stdio.h>
+# include<conio.h>
+# include<stdlib.h>
+
+void sort(int *arr,int num){
+    for(int i=0;i<num;++i){
+        for(int j=i+1;j<num;++j){
+            if(arr[i]>arr[j]){
+                int temp;
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
+
+void print_array(int *arr,int num){
+    for(int i=0;i<num;i++){
+    printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main () {
+    system("cls");
+    int nums[5]={90,33,94,11,48};
+    int size=5;
+    print_array(nums,size);
+    sort(nums,size);
+    print_array(nums,size);
+    getch();
+    system("cls");
+    return 0;
+}
